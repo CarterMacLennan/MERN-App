1cea12e6f361dbd91ad20922e3f375d30d0710f7
Carter MacLennan <carter.maclennan@verosource.com>
2020-10-26 14:26:25 -0300

    Added bootstrap navbar with associated branding and plus button

---
+++ Client/app/src/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <div>
+            <nav className="navbar bg-dark navbar-dark">
+                <span className="navbar-brand mb-0 h1 text-warning">//todo</span>
+                <button className="btn btn-md btn-warning" type="button"><i class="fas fa-plus"></i></button>
+            </nav>
+        </div>
+    )
+}
