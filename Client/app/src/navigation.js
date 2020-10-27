26a5ee25f7bc8fff061d26e91a3308ba72912214
Carter MacLennan <carter.maclennan@verosource.com>
2020-10-26 22:53:32 -0300

    Added delete button to navigation bar

--- Client/app/src/navigation.js
+++ Client/app/src/navigation.js
@@ -5,7 +5,10 @@ export default function Navigation() {
         <div>
             <nav className="navbar bg-dark navbar-dark">
                 <span className="navbar-brand mb-0 h1 text-warning">//todo</span>
-                <button className="btn btn-md btn-warning" type="button"><i class="fas fa-plus"></i></button>
+                <span>
+                    <button className="btn btn-md btn-secondary" type="button"><i class="far fa-trash-alt"></i></button>
+                    <button className="btn btn-md btn-warning" type="button"><i class="fas fa-plus "></i></button>
+                </span>
             </nav>
         </div>
     )
