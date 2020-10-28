55fd745b0f7f052cc46f1528800fce361fdb7046
Carter MacLennan <carter.maclennan@verosource.com>
2020-10-28 20:03:10 -0300

    Fixed header to start at the beginning

--- Client/app/src/card.js
+++ Client/app/src/card.js
@@ -7,11 +7,9 @@ export default function Card(props){
         <div class="card border-dark mb-3">
         <a class="close" href="#">×</a>
             <div class="card-header">
-                <div class="row justify-content-end">
-                    <div class="col-10">
-                        <h4 class="card-title"><strong>{props.project.information.title}</strong></h4>
-                    </div>
-                </div>                
+                <div class="row ">
+                    <h4 class="card-title"><strong>{props.project.information.title}</strong></h4>
+                </div>
             </div>
             
             <div class="card-body text-dark">
