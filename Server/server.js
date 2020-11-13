500464b321ea1213aaeab044b4bda10c403580a2
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-12 21:33:06 -0400

    Setting up scripts and env vars for heroku

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const db = "mongodb://localhost:27017/MERN";
 
-mongoose.connect(db, ({useUnifiedTopology: true, useNewUrlParser: true}))
+mongoose.connect(process.env.MONGO_URL || db, ({useUnifiedTopology: true, useNewUrlParser: true}))
     .then(console.log("Connected to MongoDB..."))
     .catch(err => console.log(err));
 
@@ -63,6 +63,10 @@ app.delete("/notes/delete/:id", async (req, res) => {
     }
 });
 
-app.listen(5000, ()=>{
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('Client/app/build'))
+}
+
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server is running...");
 });
