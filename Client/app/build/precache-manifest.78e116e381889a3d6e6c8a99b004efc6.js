5d540fccda2a41db809a2560ade92960b86eb464
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-14 15:29:17 -0400

    Removed /build from .gitignore

---
+++ Client/app/build/precache-manifest.78e116e381889a3d6e6c8a99b004efc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "befcea0ed17a714ecf7efb04a642b718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e86929e23341d1c8290",
+    "url": "/static/css/main.f54fa903.chunk.css"
+  },
+  {
+    "revision": "0e39ac963fc5b08290f0",
+    "url": "/static/js/2.3699fcea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3699fcea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e86929e23341d1c8290",
+    "url": "/static/js/main.978a8c02.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
